5664b763d2ab80c401d38f16341280d8427da360
Fabiano César <fcfabiano.cesar@gmail.com>
2022-02-27 19:33:41 -0300

    Define variavel para contar os minutos e segundos

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import { useState } from 'react'
 function App() {
   const [secondsAmount, setSecondsAmount] = useState(25 * 60); // 25 minutos
 
+  const minutes = Math.floor(secondsAmount / 60);
+  const seconds = secondsAmount % 60;
+
   return (
-    <h1>Contador Pomodoro</h1>
-    <span>25</span>
-    <span>:</span>
-    <span>00</span>
+    <>
+      <h1>Contador Pomodoro</h1>
+      <span>{minutes}</span>
+      <span>:</span>
+      <span>{seconds}</span>
+    </>
   )
 }
 
